2866cff0b7675263df3e96ed25487e2a970eadc5
CLog <88345999+CLOG9@users.noreply.github.com>
2023-11-28 22:15:39 +0100

    hough presper

--- main.py
+++ main.py
@@ -1,192 +1,226 @@
 # import cv2
 # import numpy as np
-# import utlis
+
+# def main():
+#     # Load the image
+#     input_path = "./0.jpg"
+#     image = cv2.imread(input_path)
+#     h = image.shape[0]
+#     w= image.shape[1]
+#     target_resolution = (round(w* 50 /100) , round(h * 50 /100))
+#     image =  cv2.resize(image, target_resolution)
+#     # Convert the image to grayscale
+#     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     output_path = "./gray_image.jpg"
+#     cv2.imwrite(output_path, gray_image)
+#     # Apply Gaussian blur
+#     blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+#     output_path = "./gaussian_image.jpg"
+#     cv2.imwrite(output_path, blurred_image)
 
 
-# ########################################################################
-# webCamFeed = True
-# pathImage = "./0.jpg"
+#     edged = cv2.Canny(blurred_image, 10, 20)
+#     output_path = "./canny_image.jpg"
+#     cv2.imwrite(output_path, edged)
+#     # Use the Sobel operator to find the gradient magnitude and direction
+#     sobel_x = cv2.Sobel(edged, cv2.CV_64F, 1, 0, ksize=3)
+#     sobel_y = cv2.Sobel(edged, cv2.CV_64F, 0, 1, ksize=3)
+#     gradient_magnitude = np.sqrt(sobel_x**2 + sobel_y**2)
+#     gradient_direction = np.arctan2(sobel_y, sobel_x)
+
+#     # Use the Hough transformation to find lines
+#     lines = cv2.HoughLines(gradient_magnitude.astype(np.uint8), 1, np.pi / 180, threshold=100)
+    
+#     # Draw the lines on the original image
+#     result_image = image.copy()
+#     for line in lines:
+#         rho, theta = line[0]
+#         a, b = np.cos(theta), np.sin(theta)
+#         x0, y0 = a * rho, b * rho
+#         x1, y1 = int(x0 + 1000 * (-b)), int(y0 + 1000 * (a))
+#         x2, y2 = int(x0 - 1000 * (-b)), int(y0 - 1000 * (a))
+#         cv2.line(result_image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+#     # Save the result in the current working directory
+#     output_path = "./output_image.jpg"
+#     cv2.imwrite(output_path, result_image)
+
+#     # Display the result
+#     cv2.imshow("Result", result_image)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+# if __name__ == "__main__":
+#     main()
+
+#####################################################
 
 
-# heightImg = 640
-# widthImg  = 480
-# ########################################################################
+# import cv2
+# import numpy as np
 
-# utlis.initializeTrackbars()
-# count=0
+# def main():
+#     # Load the image
+#     input_path = "./0.jpg"
+#     image = cv2.imread(input_path)
 
-# while True:
+#     # Convert the image to grayscale
+#     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-#     img = cv2.imread(pathImage)
-#     img = cv2.resize(img, (widthImg, heightImg)) # RESIZE IMAGE
-#     imgBlank = np.zeros((heightImg,widthImg, 3), np.uint8) # CREATE A BLANK IMAGE FOR TESTING DEBUGING IF REQUIRED
-#     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # CONVERT IMAGE TO GRAY SCALE
-#     imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1) # ADD GAUSSIAN BLUR
-#     thres=utlis.valTrackbars() # GET TRACK BAR VALUES FOR THRESHOLDS
-#     imgThreshold = cv2.Canny(imgBlur,thres[0],thres[1]) # APPLY CANNY BLUR
-#     kernel = np.ones((5, 5))
-#     imgDial = cv2.dilate(imgThreshold, kernel, iterations=2) # APPLY DILATION
-#     imgThreshold = cv2.erode(imgDial, kernel, iterations=1)  # APPLY EROSION
+#     # Apply Gaussian blur
+#     blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+#     # Use the Sobel operator to find the gradient magnitude
+#     gradient_magnitude = cv2.magnitude(cv2.Sobel(blurred_image, cv2.CV_64F, 1, 0, ksize=3),
+#                                        cv2.Sobel(blurred_image, cv2.CV_64F, 0, 1, ksize=3))
+    
+#     # Use the Hough transformation to find lines (probabilistic version)
+#     lines = cv2.HoughLinesP(gradient_magnitude.astype(np.uint8), 1, np.pi / 180, threshold=100, minLineLength=100, maxLineGap=10)
+#     print(len(lines))
+#     # Filter lines based on length (you can adjust the length threshold)
+#     filtered_lines = non_max_suppression(lines, threshold=100)
+#     print(len(filtered_lines))
+#     # Draw the lines on the original image
+#     result_image = image.copy()
+#     for line in filtered_lines:
+#         x1, y1, x2, y2 = line[0]
+#         cv2.line(result_image, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-#     ## FIND ALL COUNTOURS
-#     imgContours = img.copy() # COPY IMAGE FOR DISPLAY PURPOSES
-#     imgBigContour = img.copy() # COPY IMAGE FOR DISPLAY PURPOSES
-#     contours, hierarchy = cv2.findContours(imgThreshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # FIND ALL CONTOURS
-#     cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10) # DRAW ALL DETECTED CONTOURS
+#     # Save the result in the current working directory
+#     output_path = "./output_image.jpg"
+#     cv2.imwrite(output_path, result_image)
 
+#     # Display the result
+#     cv2.imshow("Result", result_image)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
 
-#     # FIND THE BIGGEST COUNTOUR
-#     biggest, maxArea = utlis.biggestContour(contours) # FIND THE BIGGEST CONTOUR
-#     if biggest.size != 0:
-#         biggest=utlis.reorder(biggest)
-#         cv2.drawContours(imgBigContour, biggest, -1, (0, 255, 0), 20) # DRAW THE BIGGEST CONTOUR
-#         imgBigContour = utlis.drawRectangle(imgBigContour,biggest,2)
-#         pts1 = np.float32(biggest) # PREPARE POINTS FOR WARP
-#         pts2 = np.float32([[0, 0],[widthImg, 0], [0, heightImg],[widthImg, heightImg]]) # PREPARE POINTS FOR WARP
-#         matrix = cv2.getPerspectiveTransform(pts1, pts2)
-#         imgWarpColored = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+# def non_max_suppression(lines, threshold):
+#     if len(lines) == 0:
+#         return []
 
-#         #REMOVE 20 PIXELS FORM EACH SIDE
-#         imgWarpColored=imgWarpColored[20:imgWarpColored.shape[0] - 20, 20:imgWarpColored.shape[1] - 20]
-#         imgWarpColored = cv2.resize(imgWarpColored,(widthImg,heightImg))
+#     # Convert lines to numpy array
+#     lines = np.array(lines).reshape(-1, 4)
 
-#         # APPLY ADAPTIVE THRESHOLD
-#         imgWarpGray = cv2.cvtColor(imgWarpColored,cv2.COLOR_BGR2GRAY)
-#         imgAdaptiveThre= cv2.adaptiveThreshold(imgWarpGray, 255, 1, 1, 7, 2)
-#         imgAdaptiveThre = cv2.bitwise_not(imgAdaptiveThre)
-#         imgAdaptiveThre=cv2.medianBlur(imgAdaptiveThre,3)
+#     # Calculate line lengths
+#     lengths = np.linalg.norm(lines[:, :2] - lines[:, 2:], axis=1)
 
-#         # Image Array for Display
-#         imageArray = ([img,imgGray,imgThreshold,imgContours],
-#                       [imgBigContour,imgWarpColored, imgWarpGray,imgAdaptiveThre])
+#     # Get indices to keep using a threshold
+#     indices_to_keep = lengths > threshold
 
-#     else:
-#         imageArray = ([img,imgGray,imgThreshold,imgContours],
-#                       [imgBlank, imgBlank, imgBlank, imgBlank])
+#     # Filter lines based on the threshold
+#     filtered_lines = lines[indices_to_keep]
 
-#     # LABELS FOR DISPLAY
-#     lables = [["Original","Gray","Threshold","Contours"],
-#               ["Biggest Contour","Warp Prespective","Warp Gray","Adaptive Threshold"]]
+#     return filtered_lines.reshape(-1, 1, 4).tolist()
 
-#     stackedImage = utlis.stackImages(imageArray,0.75,lables)
-#     cv2.imshow("Result",stackedImage)
+# if __name__ == "__main__":
+#     main()
 
-#     # SAVE IMAGE WHEN 's' key is pressed
-#     if cv2.waitKey(1) & 0xFF == ord('s'):
-#         cv2.imwrite(str(count)+".jpg",imgWarpColored)
-#         cv2.rectangle(stackedImage, ((int(stackedImage.shape[1] / 2) - 230), int(stackedImage.shape[0] / 2) + 50),
-#                       (1100, 350), (0, 255, 0), cv2.FILLED)
-#         cv2.putText(stackedImage, "Scan Saved", (int(stackedImage.shape[1] / 2) - 200, int(stackedImage.shape[0] / 2)),
-#                     cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
-#         cv2.imshow('Result', stackedImage)
-#         cv2.waitKey(300)
-#         count += 1
+## didnt find the quads yet
 
 
-
+####################################################################################
 
 
 import cv2
 import numpy as np
-from imutils.perspective import four_point_transform
-import pytesseract
+import math
 
-# cap = cv2.VideoCapture(0 + cv2.CAP_DSHOW)
+def main():
+    # Load the image
+    input_path = "./0.jpg"
+    image = cv2.imread(input_path)
 
-pathImage = "./0.jpg"
+    # Convert the image to grayscale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-cap =cv2.imread(pathImage)
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+    # Apply Gaussian blur
+    blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+    # Use the Sobel operator to find the gradient magnitude
+    gradient_magnitude = cv2.magnitude(cv2.Sobel(blurred_image, cv2.CV_64F, 1, 0, ksize=3),
+                                       cv2.Sobel(blurred_image, cv2.CV_64F, 0, 1, ksize=3))
+    
+    # Use the Hough transformation to find lines (probabilistic version)
+    lines = cv2.HoughLinesP(gradient_magnitude.astype(np.uint8), 1, np.pi / 180, threshold=100, minLineLength=100, maxLineGap=10)
+    print(len(lines))
+    # Filter lines based on length (you can adjust the length threshold)
+    filtered_lines = non_max_suppression(lines, threshold=100)
+    print(len(filtered_lines))
 
-count = 0
-scale = 0.5
+    best_quadrilateral = find_best_quadrilateral(filtered_lines)
+    # Draw the lines on the original image
+    result_image = image.copy()
+    for line in best_quadrilateral:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(result_image, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-font = cv2.FONT_HERSHEY_SIMPLEX
+    # Save the result in the current working directory
+    output_path = "./output_image.jpg"
+    cv2.imwrite(output_path, result_image)
 
-WIDTH, HEIGHT = 480, 640
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
-cap = cv2.resize(cap, (WIDTH, HEIGHT)) # RESIZE IMAGE
+    # Display the result
+    cv2.imshow("Result", result_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
+def non_max_suppression(lines, threshold):
+    if len(lines) == 0:
+        return []
 
-def image_processing(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, threshold = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
+    # Convert lines to numpy array
+    lines = np.array(lines).reshape(-1, 4)
 
-    return threshold
+    # Calculate line lengths
+    lengths = np.linalg.norm(lines[:, :2] - lines[:, 2:], axis=1)
 
+    # Get indices to keep using a threshold
+    indices_to_keep = lengths > threshold
 
-def scan_detection(image):
-    global document_contour
+    # Filter lines based on the threshold
+    filtered_lines = lines[indices_to_keep]
 
-    document_contour = np.array([[0, 0], [WIDTH, 0], [WIDTH, HEIGHT], [0, HEIGHT]])
-
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    _, threshold = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    contours, _ = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)
-
-    max_area = 0
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        if area > 1000:
-            peri = cv2.arcLength(contour, True)
-            approx = cv2.approxPolyDP(contour, 0.015 * peri, True)
-            if area > max_area and len(approx) == 4:
-                document_contour = approx
-                max_area = area
-
-    cv2.drawContours(frame, [document_contour], -1, (0, 255, 0), 3)
-
-
-def center_text(image, text):
-    text_size = cv2.getTextSize(text, font, 2, 5)[0]
-    text_x = (image.shape[1] - text_size[0]) // 2
-    text_y = (image.shape[0] + text_size[1]) // 2
-    cv2.putText(image, text, (text_x, text_y), font, 2, (255, 0, 255), 5, cv2.LINE_AA)
+    return filtered_lines.reshape(-1, 1, 4).tolist()
 
 
-while True:
+def find_best_quadrilateral(lines):
+    best_score = -1
+    best_quadrilateral = []
 
-    # _, frame = cap.read()
-    frame = cv2.rotate(cap, cv2.ROTATE_180)
-    frame_copy = frame.copy()
+    # Iterate through every combination of four lines
+    for i in range(len(lines)):
+        for j in range(i+1, len(lines)):
+            for k in range(j+1, len(lines)):
+                for l in range(k+1, len(lines)):
+                    quadrilateral = [lines[i][0], lines[j][0], lines[k][0], lines[l][0]]
+                    score = heuristic_scoring_function(quadrilateral)
 
-    scan_detection(frame_copy)
+                    # Update the best quadrilateral if the score is higher
+                    if score > best_score:
+                        best_score = score
+                        best_quadrilateral = quadrilateral
 
-    cv2.imshow("input", cv2.resize(frame, (int(scale * WIDTH), int(scale * HEIGHT))))
+    return best_quadrilateral
 
-    warped = four_point_transform(frame_copy, document_contour.reshape(4, 2))
-    cv2.imshow("Warped", cv2.resize(warped, (int(scale * warped.shape[1]), int(scale * warped.shape[0]))))
 
-    processed = image_processing(warped)
-    processed = processed[10:processed.shape[0] - 10, 10:processed.shape[1] - 10]
-    cv2.imshow("Processed", cv2.resize(processed, (int(scale * processed.shape[1]),
-                                                   int(scale * processed.shape[0]))))
+def heuristic_scoring_function(quadrilateral):
+    # Calculate angles between lines in the quadrilateral
+    angles = []
+    for i in range(4):
+        p1 = np.array(quadrilateral[i][:2])
+        p2 = np.array(quadrilateral[(i + 1) % 4][:2])
+        p3 = np.array(quadrilateral[(i + 2) % 4][:2])
 
-    pressed_key = cv2.waitKey(1) & 0xFF
-    if pressed_key == 27:
-        break
+        vector1 = p1 - p2
+        vector2 = p3 - p2
 
-    elif pressed_key == ord('s'):
-        cv2.imwrite("scanned_" + str(count) + ".jpg", processed)
-        count += 1
+        dot_product = np.dot(vector1, vector2)
+        norm_product = np.linalg.norm(vector1) * np.linalg.norm(vector2)
 
-        center_text(frame, "Scan Saved")
-        cv2.imshow("input", cv2.resize(frame, (int(scale * WIDTH), int(scale * HEIGHT))))
-        cv2.waitKey(500)
+        angle = math.degrees(math.acos(dot_product / norm_product))
+        angles.append(angle)
 
-    elif pressed_key == ord('o'):
-        file = open("recognized_" + str(count - 1) + ".txt", "w")
-        ocr_text = pytesseract.image_to_string(warped)
-        # print(ocr_text)
-        file.write(ocr_text)
-        file.close()
+    # Score based on the sum of angles
+    score = sum(angles)
 
-        center_text(frame, "Text Saved")
-        cv2.imshow("input", cv2.resize(frame, (int(scale * WIDTH), int(scale * HEIGHT))))
-        cv2.waitKey(500)
+    return score
 
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
